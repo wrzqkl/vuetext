bab72d894e24ae3e6e8679f792e5986726ce0fd4
wrzqkl <1292096002.com>
2025-11-03 11:38:18 +0800

    xin

---
+++ src/components/watchzhenting.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="watchzhenting">
+        <hr>
+        <h3>watch监听</h3>
+        <p>
+            {{ message }}
+        </p>
+        <button @click="update">修改数据</button>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'watchzhenting',
+    data() {
+        return {
+            message: 'watchzhenting'
+        }
+    },
+    methods: {
+        update() {
+            this.message = '11111111111111111update';
+        }
+    },
+    watch: {
+        // watch 里的函数名称必须与 data 中的属性名称相同，newValue 是修改后的值，oldValue 是修改前的值，这两个都是参数，是必须的
+        message(newValue, oldValue){
+            // 数据发生变化，自动执行的函数
+            console.log(newValue, '+++++++++++++');
+            console.log(oldValue, '-------------');
+        }
+    }
+};
+</script>
+  
+<style scoped>
+.watchzhenting {
+    color: blueviolet;
+}
+</style>
